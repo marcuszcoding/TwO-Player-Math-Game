c7716247af7eadc3fc5e8aa3596095d160ff0564
marcuszcoding <marcuszcoding@gmail.com>
2023-05-01 14:51:15 -0600

    finish question class

--- question.rb
+++ question.rb
@@ -1,10 +1,15 @@
 #Question psuedo 
 # Question needs to be read, needs a question with 2 random numbers and something to check an answer?
 # Start by making a question class, have a reader for the quesiton/answer
+# make 2 variables for random numbers? then put inside of question string, answer is num 1 + num 2.
 
 class Question
   attr_reader :question, answer
-   def initialize()
+  def initialize()
+    ranNumber1 = rand(1..20)
+    ranNumber2 = rand(1..20)
+    @question = "What does #{ranNumber1} + #{ranNumber2} equal?"
+    @answer = ranNumber1 + ranNumber2
   end
  end
  
