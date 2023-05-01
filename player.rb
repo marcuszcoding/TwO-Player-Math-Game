e167b66bfd8e723f4156e3da61abb11060b4fc11
marcuszcoding <marcuszcoding@gmail.com>
2023-05-01 14:31:17 -0600

    pseudo code ideas

--- player.rb
+++ player.rb
@@ -1,1 +1,4 @@
-
+# What does a player need?
+# A player needs to have a name, lives and something to say what is their turn or not?
+# We only need to read the player name and have access to change lives and turn status
+# So total we need to access lives/turn, read name, then initalize a name with an @name, @lives and @turn
