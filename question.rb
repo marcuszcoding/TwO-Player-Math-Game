fec695993e46340f4780f594015381a881148738
marcuszcoding <marcuszcoding@gmail.com>
2023-05-01 14:53:05 -0600

    finish question class with inline comments

--- question.rb
+++ question.rb
@@ -1,15 +1,10 @@
-#Question psuedo 
-# Question needs to be read, needs a question with 2 random numbers and something to check an answer?
-# Start by making a question class, have a reader for the quesiton/answer
-# make 2 variables for random numbers? then put inside of question string, answer is num 1 + num 2.
-
 class Question
-  attr_reader :question, answer
+  attr_reader :question, answer # reads our question and answer
   def initialize()
-    ranNumber1 = rand(1..20)
+    ranNumber1 = rand(1..20) # random number with the specified range of 1 to 20 for the assignment
     ranNumber2 = rand(1..20)
-    @question = "What does #{ranNumber1} + #{ranNumber2} equal?"
-    @answer = ranNumber1 + ranNumber2
+    @question = "What does #{ranNumber1} + #{ranNumber2} equal?" # question with random numbers incorporated
+    @answer = ranNumber1 + ranNumber2 # saves our answer for each question 
   end
  end
  
