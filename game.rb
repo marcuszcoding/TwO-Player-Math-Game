dfa29fec7e5fdf0f0ab5a4d1e7cc1e281800ed1c
marcuszcoding <marcuszcoding@gmail.com>
2023-05-01 15:00:14 -0600

    initial idea for player in game class

--- game.rb
+++ game.rb
@@ -1,5 +1,16 @@
 # Setting up the game
 # 1.) Going to need to require our player and question to make the game
+# 2.) Need to set up a game class that initalizes intro step
+#   - First steps we need to record players names with gets.chomp
+#   - After recieving terminal input set the player 
 
 require './player.rb'
 require './question.rb'
+
+class Game
+  def initalize
+    puts "What is players name?"
+    player = gets.chomp
+    @player = Player.new(player)
+  end
+end 
