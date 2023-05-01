2e1594ba4f477d5a129aab7fff64b94af4d114be
marcuszcoding <marcuszcoding@gmail.com>
2023-05-01 14:34:40 -0600

    finish player class

--- player.rb
+++ player.rb
@@ -2,3 +2,14 @@
 # A player needs to have a name, lives and something to say what is their turn or not?
 # We only need to read the player name and have access to change lives and turn status
 # So total we need to access lives/turn, read name, then initalize a name with an @name, @lives and @turn
+
+class Player
+  attr_accessor :lives, :turn
+  attr_reader :name
+   def initialize(name)
+    @name = name
+    @lives = 3
+    @turn = false
+  end
+ end
+ 
