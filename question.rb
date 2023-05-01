6af382bc3d81f9e9b690c14c1bd24002c66dee37
marcuszcoding <marcuszcoding@gmail.com>
2023-05-01 14:47:26 -0600

    add initial question class setup

---
+++ question.rb
@@ -0,0 +1,10 @@
+#Question psuedo 
+# Question needs to be read, needs a question with 2 random numbers and something to check an answer?
+# Start by making a question class, have a reader for the quesiton/answer
+
+class Question
+  attr_reader :question, answer
+   def initialize()
+  end
+ end
+ 
