02805835429f34ba033df2122f83d6684ecf6ab3
marcuszcoding <marcuszcoding@gmail.com>
2023-05-02 09:50:20 -0600

    add snake_case vs camelCase

--- player.rb
+++ player.rb
@@ -1,12 +1,13 @@
 class Player
   attr_reader :name, :lives # reads name
-   def initialize(name) # initalize name
+  
+  def initialize(name) # initalize name
     # setting values
     @name = name
     @lives = 3
   end
 
-  def loseLife # func for losing a life when answer is wrong
+  def lose_life # func for losing a life when answer is wrong
     @lives -= 1
   end 
 
