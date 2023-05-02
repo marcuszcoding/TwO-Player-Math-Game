89c8cd6c3af14a0d71304b951b010eaf6aedc480
marcuszcoding <marcuszcoding@gmail.com>
2023-05-02 09:49:54 -0600

    change players to hardcoded and set turn to player 1

--- game.rb
+++ game.rb
@@ -3,14 +3,17 @@
 # 2.) Need to set up a game class that initalizes intro step
 #   - First steps we need to record players names with gets.chomp
 #   - After recieving terminal input set the player 
+# 3.) Need to make a question in the game (so make a func) and then check their answer
+#   - In create question we need to create question, puts the correct message 
 
 require './player.rb'
 require './question.rb'
 
 class Game
+
   def initalize
-    puts "What is players name?"
-    player = gets.chomp
-    @player = Player.new(player)
+    @player1 = Player.new('Player 1')
+    @player2 = Player.new('Player 2')
+    @player_turn = @player1
   end
 end 
